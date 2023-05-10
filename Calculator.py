c849b908830fab21c6ee4e921ad445c949ed13e5
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-10 10:38:17 +0800

    random

--- Calculator.py
+++ Calculator.py
@@ -4,10 +4,29 @@
  and division. This program also exhibits exceptionhandling or catching errors using 
  appropriate exceptions.'''
  
- '''pseudocode
- HEADING/PROMPT
- GET user's input (numbers)
- ASSIGN variable name to the inputs and operation
- Use MATCH case statements
- match operations
- '''
+from pyfiglet import figlet_format
+from termcolor import colored
+from colorama import Back, Fore, Style, init
+import time
+
+from tkinter import *
+from tkinter import simpledialog
+from tkinter import messagebox
+ws = Tk()
+ws.title("SIMPLE CALCULATOR")
+
+def add():
+ while True: 
+    try:  
+            num1 = simpledialog.askinteger("Enter a number: ")
+            num2 = simpledialog.askinteger("Enter a second number: ")
+     if num1 and num2 is not None:
+            
+     else:
+           print("You don't have a first name?")
+            parent=ws,
+    except:  
+            messagebox.showerror('Error Encountered', 'Error: Please enter a valid input!')
+        finally
+            
+            
