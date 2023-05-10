2c789d99b578de16edc69f15de494d3ba4b68e77
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-10 08:48:32 +0800

    program description

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,5 @@
 # BABIERA,ALEXA | CMPE-103-MODULE-2-FILE-HANDLING-IN PYTHON | PROGRAMMING EXERCISE 4
+'''This program asks the user for input (integers) and calculate them using 
+ user's choice of mathematical operation such as addition, subtraction, multiplication 
+ and division. This program also exhibits exceptionhandling or catching errors using 
+ appropriate exceptions.'''
