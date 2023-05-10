b8b4767e1c019ba2b3219fe34b5f8542a6d412a1
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-10 09:09:34 +0800

    Pseudocode

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,11 @@
  user's choice of mathematical operation such as addition, subtraction, multiplication 
  and division. This program also exhibits exceptionhandling or catching errors using 
  appropriate exceptions.'''
+ 
+ '''pseudocode
+ HEADING/PROMPT
+ GET user's input (numbers)
+ ASSIGN variable name to the inputs and operation
+ Use MATCH case statements
+ match operations
+ '''
